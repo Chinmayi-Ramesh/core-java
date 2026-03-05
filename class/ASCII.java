ada8b943cb09ab7013d35f4314fc69a6ef4a5817
Chinmayi Ramesh <chinmayigr99@gmail.com>
2026-03-05 10:33:28 +0530

    ClassExamples

---
+++ class/ASCII.java
@@ -0,0 +1,6 @@
+class ASCII {
+    public static void main(String[] args) {
+        char gender = 'A' + '0';
+        System.out.println(gender);
+    }
+}
---
+++ class/Baba.java
@@ -0,0 +1,5 @@
+class Baba {
+	public static void main(String[] args){
+		System.out.println("Baba" + "23" + "67");
+	}
+}
